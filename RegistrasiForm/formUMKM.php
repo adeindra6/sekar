40491d85ecc85eccf310511adf36702611cabc24
raufendro-dev <raufendro@gmail.com>
2024-06-25 06:43:32 +0700

    update

--- RegistrasiForm/formUMKM.php
+++ RegistrasiForm/formUMKM.php
@@ -58,7 +58,7 @@
           <div class="user-input-box">
             <label for="lokasi">Kota/Kabupaten</label>
             <select name="lokasi" id="lokasi">
-              <option value="Seleman">Seleman</option>
+              <option value="Sleman">Sleman</option>
               <option value="Bantul">Bantul</option>
               <option value="Jogja">Jogja</option>
             </select>
