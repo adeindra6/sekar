4cf1272b2a3dfb871b61922bf02cf0b6fbe3c44b
Ade Indra Pratama <adeprathama@gmail.com>
2024-06-24 20:46:53 +0700

    Add koneksi to db

--- config/koneksi.php
+++ config/koneksi.php
@@ -6,5 +6,6 @@ $koneksi = mysqli_connect("103.193.178.139", "user_sdtb", "ngentotBlok69@", "hea
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
+echo "Koneksi berhasil";
  
 ?>
